f802da8e07794948fc6480bd01ec4a96df6b4d35
Dinesh <dinesh.akkisetty@moengage.com>
2025-04-09 12:49:27 +0530

    DP-946 Update Unit Tests

--- debezium-connector-mongodb/src/test/java/io/debezium/connector/mongodb/TopicSelectorTest.java
+++ debezium-connector-mongodb/src/test/java/io/debezium/connector/mongodb/TopicSelectorTest.java
@@ -32,14 +32,14 @@ public class TopicSelectorTest {
     @Test
     public void shouldHandleCollectionIdWithDatabaseAndCollection() {
         assertTopic(noPrefix, dbAndCollection("db", "coll")).isEqualTo("coll");
-        assertTopic(withPrefix, dbAndCollection("db", "coll")).isEqualTo("prefix.db.coll");
+        assertTopic(withPrefix, dbAndCollection("db", "coll")).isEqualTo("prefix.coll");
     }
 
     @Test
     @FixFor("DBZ-878")
     public void shouldHandleCollectionIdWithInvalidTopicNameChar() {
         assertTopic(noPrefix, dbAndCollection("db", "my@collection")).isEqualTo("my_collection");
-        assertTopic(withPrefix, dbAndCollection("db", "my@collection")).isEqualTo("prefix.db.my_collection");
+        assertTopic(withPrefix, dbAndCollection("db", "my@collection")).isEqualTo("prefix.my_collection");
     }
 
     protected StringAssert assertTopic(TopicSelector<CollectionId> selector, CollectionId collectionId) {
